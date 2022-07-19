22285e7bb17fbf14bede0a6053657c42eace7a2f
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-07-19 20:57:17 +0300

    Initial Commit

---
+++ WebBrauser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrauser
+//
+//  Created by Алексей Моторин on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
